264f8a5d35690762c9c34c2f3240eca893a91ee2
vagrant <vagrant@homestead>
2018-04-21 19:55:25 +0200

    Adds Package base files

---
+++ src/Pixame.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aubreykodar
+ * Date: 2018/04/21
+ * Time: 7:54 PM
+ */
+
+namespace AubreyKodar\Pixame;
+
+
+class Pixame
+{
+	public function __construct() {
+
+	}
+}
