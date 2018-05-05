7010690fc4865d88b49898d9e73f513416311456
vagrant <hlungwaneak@protonmail.com>
2018-05-05 15:25:33 +0200

    Adds Paginagtion

--- src/Pixame.php
+++ src/Pixame.php
@@ -12,11 +12,11 @@ class Pixame{
 		$this->key = $key;
 		$this->httpClient = new GuzzleHttp\Client();
    }
-   public function Search($keyword, $ReturnArray = false,$type = 'photo'){
+   public function Search($keyword, $ReturnArray = false,$page = 1, $per_page = 20,$type = 'photo'){
 
    	try{
 
-   		$resp =  $this->httpClient->get($this->baseUrl.'?key='.$this->key.'&q='.urlencode($keyword).'&image_type='.$type);
+   		$resp =  $this->httpClient->get($this->baseUrl.'?key='.$this->key.'&q='.urlencode($keyword).'&image_type='.$type.'&page='.$page.'&per_page'.$per_page);
 
 	    if($resp->getStatusCode() == 200){
 	    	return GuzzleHttp\json_decode($resp->getBody(), $ReturnArray);
@@ -49,5 +49,16 @@ class Pixame{
     }catch (GuzzleHttp\Exception\ClientException $exception){
 	    throw new Exceptions\PixameException($exception->getMessage(),$exception->getCode());
     }
+   }
+   public function NextPage(){
+
+   	try{
+
+
+
+    }catch (GuzzleHttp\Exception\ClientException $exception){
+	    throw new Exceptions\PixameException($exception->getMessage(),$exception->getCode());
+    }
+
    }
 }
